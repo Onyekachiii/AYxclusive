812116c3ca62f0064fa40b1dc69469dae2b897a3
Onyekachiii <stiloonwa4u@yahoo.com>
2023-11-29 16:09:07 +0400

    Used DontEnv

--- ayxclusive/settings.py
+++ ayxclusive/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,12 +149,13 @@ JAZZMIN_SETTINGS = {
 
 AUTH_USER_MODEL = 'userauths.User'
 
+load_dotenv()
 
 # Django Email Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ayexclusive1@outlook.com'
-EMAIL_HOST_PASSWORD = 'exclusive321'
-EMAIL_USE_TSL = True
-EMAIL_USE_SSL = False
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TSL = os.getenv('EMAIL_USE_TSL')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
